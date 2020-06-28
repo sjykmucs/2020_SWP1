cff73fd22c291d21df613389d0c0bed84bc9c325
sjykmucs <star01070@kookmin.ac.kr>
2020-06-28 20:04:44 +0900

    Issue #7 Modified calc.py : change error output

--- calc/calc.py
+++ calc/calc.py
@@ -6,10 +6,13 @@ def application(environ, start_response):
     first_num = d.get('first_num', [''])[0]
     second_num = d.get('second_num', [''])[0]
     sum, mul = 0, 0
-    if '' not in [first_num, second_num]:
+    try :
         first_num, second_num = int(first_num), int(second_num)
         sum = first_num + second_num
         mul = first_num * second_num
+    except ValueError :
+        sum = -1
+        mul = -1
     response_body = html % {'sum':sum, 'mul':mul}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
