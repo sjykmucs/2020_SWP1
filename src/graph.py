c65897efcbc9ec33f44b6169a2c6d910cb63f9b1
sjykmucs <star01070@kookmin.ac.kr>
2020-06-28 15:09:14 +0900

    Issue #3 Modified graph.py

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,5 @@
 from cgi import parse_qs
+
 from template import html
 
 import matplotlib as mpl
@@ -9,7 +10,7 @@ import matplotlib.pyplot as plt
 def application(environ, start_response):
     if environ['PATH_INFO'] == '/img/graph.png':
         try:
-            with open('./img/graph.png', 'rb') as f:
+            with open('../img/graph.png', 'rb') as f:
                 response_body = f.read()
         except:
             response_body = ''
